b7dc372dc610807645c52513ce49950a5b416b97
unknown <nikita.waara@hotmail.com>
2021-09-17 18:54:39 +0300

    saving changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Statistics = (props) => {
+      if (props.good.lenght === 0 && props.neutral.lenght === 0 && props.bad.lenght === 0) {
+        return (
+          <div>No feedback given</div>
+        )
+      }
+  return(
+      <div>
+        <div>
+        <div>Good: {props.good}</div>
+        <div>Neutral: {props.neutral}</div>
+        <div>Bad: {props.bad}</div>
+        </div>
+      </div>
+
+    )
+  
+
+  
 }
+
+const Button = ({handleClick, text}) => (
+  <button onClick = {handleClick}>{text}</button>
+)
+
+
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  //const [stats, setAll] = useState([]);
+
+
+const handleGoodClick = () => {
+  setGood(good + 1)
+  }
+
+
+const handleBadClick = () => {
+  setBad(bad + 1)
+  }
+
+const handleNeutralClick = () => {
+  setNeutral(neutral + 1)
+  }
+
+
+return(
+  <div>
+    <h1>Give us feedback</h1>
+
+    <div>
+      <Button handleClick={handleGoodClick} text='Good'/>
+
+      <Button handleClick={handleNeutralClick} text='Neutral'/>
+
+      <Button handleClick={handleBadClick} text='Bad'/>
+    </div>
+
+      <h2>Feedback history:</h2>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    
+    </div>
+)
+
+}
+
+
 
 export default App;
